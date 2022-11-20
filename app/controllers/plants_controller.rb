f06f315bbae82c5fb8489221e178b662af93f3d6
+ <+>
2022-11-20 16:40:46 +0300

    complete

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,14 +8,28 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
   # POST /plants
   def create
-    plant = Plant.create(plant_params)
+    plant =  Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  # PATCH /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
   end
 
   private
@@ -23,4 +37,8 @@ class PlantsController < ApplicationController
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
 end
+ 
